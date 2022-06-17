5001f1a92a70fe2db8857c40d0cea8a52f15357b
Pana <vip_P4na@protonmail.com>
2022-06-17 12:45:14 +0200

    video 1:49 rivedere meglio i filter

--- siteTest/pages/views.py
+++ siteTest/pages/views.py
@@ -1,9 +1,14 @@
+from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request,*args, **kwargs):
+    context = {
+        "username":request.user,
+        "numList":[12,3585,15,83,122,53,846]
+    }
+    return render(request, "home.html", context)
 
 def contact_view(*args, **kwargs):
     return HttpResponse("<h1>Contact</h1>")
